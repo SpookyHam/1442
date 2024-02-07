64ce4da75e50a4ab07a873281ccec150d56fb1fe
SpookyHam <dominoes321@gmail.com>
2024-02-07 14:32:13 -0500

    Added Scripts for Player Movement and Shooting

---
+++ 1442/Assets/Scripts/PlayerArrowBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerArrowBehavior : MonoBehaviour
+{
+    public float arrowLimit = 11f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * 8f);
+    
+
+    if (transform.position.y > arrowLimit || transform.position.y < -arrowLimit)
+    {
+        Destroy(this.gameObject);
+    }
+    }
+}
